e5e25c8cf06da019b3d88d327f5d6fbae870def3
VikNamin <aburnyashev@list.ru>
2022-07-08 07:17:20 +0400

    Создание PolicyActivity, чтение из файла policy.txt

--- app/src/main/java/ru/vik/trueworldcupproject/ForthActivity.java
+++ app/src/main/java/ru/vik/trueworldcupproject/ForthActivity.java
@@ -2,14 +2,62 @@
 package ru.vik.trueworldcupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class ForthActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
+    public static ArrayList<String> yearOfWinning = new ArrayList<>();
+    public static ArrayList<String> countryOfWinning = new ArrayList<>();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forth);
+        initialize();
+        recyclerView = findViewById(R.id.forthActivityRecyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        RecyclerView.Adapter<WinnersAdapter.ViewHolder> adapter = new WinnersAdapter(this, yearOfWinning, countryOfWinning);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initialize(){
+        yearOfWinning.add("2018");
+        yearOfWinning.add("2014");
+        yearOfWinning.add("2010");
+        yearOfWinning.add("2006");
+        yearOfWinning.add("2002");
+        yearOfWinning.add("1998");
+        yearOfWinning.add("1994");
+        yearOfWinning.add("1990");
+        yearOfWinning.add("1986");
+        yearOfWinning.add("1982");
+        yearOfWinning.add("1978");
+        yearOfWinning.add("1974");
+        yearOfWinning.add("1970");
+        yearOfWinning.add("1966");
+
+        countryOfWinning.add("France");
+        countryOfWinning.add("Germany");
+        countryOfWinning.add("Spain");
+        countryOfWinning.add("Italy");
+        countryOfWinning.add("Brazil");
+        countryOfWinning.add("France");
+        countryOfWinning.add("Brazil");
+        countryOfWinning.add("Germany");
+        countryOfWinning.add("Argentina");
+        countryOfWinning.add("Italy");
+        countryOfWinning.add("Argentina");
+        countryOfWinning.add("Germany");
+        countryOfWinning.add("Brazil");
+        countryOfWinning.add("England");
     }
 }
--- app/src/main/java/ru/vik/trueworldcupproject/PolicyActivity.java
+++ app/src/main/java/ru/vik/trueworldcupproject/PolicyActivity.java
@@ -3,12 +3,52 @@ package ru.vik.trueworldcupproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class PolicyActivity extends AppCompatActivity {
+
+    private final static int FILE_PATH = R.raw.policy;
+    TextView policyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_policy);
+
+        policyTextView = findViewById(R.id.policyTextView);
+        openFile(FILE_PATH);
+    }
+
+    private void openFile(int filePath) {
+        try {
+            InputStream inputStream = getResources().openRawResource(filePath);
+
+            if (inputStream != null) {
+                InputStreamReader isr = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(isr);
+                String line;
+                StringBuilder builder = new StringBuilder();
+
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line + "\n");
+                }
+
+                inputStream.close();
+                policyTextView.setText(builder.toString());
+            }
+        }
+        catch (Throwable t) {
+            Toast.makeText(getApplicationContext(),
+                    "Exception: " + t.toString(),
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
